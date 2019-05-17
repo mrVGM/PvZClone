daecebda847e861b51bf7b8ca4badd3ed8f427c0
Vasil <magaranov@abv.bg>
2019-05-18 00:39:45 +0300

    separated program coroutines

--- Server/Assets/Scripts/ProgramsAPI/program.js
+++ Server/Assets/Scripts/ProgramsAPI/program.js
@@ -11,6 +11,7 @@ var program = {
     createInstance: function () {
         var inst = {
             name: 'Program',
+            stop: false,
             params: {
                 updateTime: {
                     name: 'Update Time',
@@ -19,12 +20,41 @@ var program = {
                 },
             },
             interface: {
-                createCoroutine: function (inst) {
-                    var counter = 0;
+                coroutine: function (inst) {
+                    var cnt = 0;
                     function crt() {
-                        console.log(counter++);
-                        if (counter < 20)
+                        console.log(cnt++);
+                        if (cnt < 20)
                             return crt;
+                    }
+                    return crt;
+                },
+                finish: function (inst) {
+                    var cnt = 0;
+                    function crt() {
+                        console.log(cnt++);
+                        if (cnt < 20)
+                            return crt;
+                    }
+                    return crt;
+                },
+                createCoroutine: function (inst) {
+                    var c = inst.interface.coroutine(inst);
+                    var f = inst.interface.finish;
+                    var permanentStop = false;
+                    function crt() {
+                        if (!permanentStop && !inst.stop && c) {
+                            c = c();
+                            return crt;
+                        }
+                        if (!permanentStop) {
+                            f = f(inst);
+                        }
+                        permanentStop = true;
+                        if (f) {
+                            f = f();
+                            return crt;
+                        }
                     }
                     return crt;
                 }
