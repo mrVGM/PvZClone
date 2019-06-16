c1cfeec2b052922dcd4883ba35ab06004442069d
Vasil <magaranov@abv.bg>
2019-06-16 17:10:01 +0300

    Added deployTileScript to the tiles

---
+++ Server/Assets/Scripts/PvZClone/deployTile.js
@@ -0,0 +1,12 @@
+var deployTile = {
+    createInstance: function() {
+        var inst = {
+            name: 'Deploy Tile',
+            params: {},
+            interface: {}
+        };
+        return inst;
+    }
+};
+
+module.exports = deployTile;
