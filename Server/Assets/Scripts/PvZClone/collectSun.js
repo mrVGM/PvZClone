b04af82b8d2c340ebe8ce162a6a9f6ea57e43a01
Vasil <magaranov@abv.bg>
2019-06-25 00:42:36 +0300

    Collecting Sun

---
+++ Server/Assets/Scripts/PvZClone/collectSun.js
@@ -0,0 +1,57 @@
+var collectSun = {
+    extendsFrom: 'Assets\\Scripts\\ProgramsAPI\\program.js',
+    createInstance: function() {
+        var inst = {
+            name: 'Collect Sun',
+            params: {
+                pointedObjectsTag: {
+                    name: 'Pointed Objects',
+                    type: 'fileObject',
+                    value: undefined
+                },
+                sunPointerTargetTag: {
+                    name: 'Sun Pointer Target tag',
+                    type: 'fileObject',
+                    value: undefined
+                }
+            },
+            interface: {
+                coroutine: function* (inst) {
+                    function getPointedSun(pointed, sunTag) {
+                        if (!pointed) {
+                            return undefined;
+                        }
+                        for (var i = 0; i < pointed.length; ++i) {
+                            if (pointed[i].params.targetType.value === sunTag) {
+                                return pointed[i];
+                            }
+                        }
+                    }
+                    while (true) {
+                        while (!game.input.mouseDown || game.input.mouseButton !== 0) {
+                            yield undefined;
+                        }
+                        var pointedTargets = inst.events[inst.params.pointedObjectsTag.value];
+                        var pointedSun = getPointedSun(pointedTargets, inst.params.sunPointerTargetTag.value);
+                        if (!pointedSun) {
+                            yield;
+                            continue;
+                        }
+                        while (game.input.mouseDown) {
+                            yield undefined;
+                        }
+                        pointedTargets = inst.events[inst.params.pointedObjectsTag.value];
+                        var sunOnMouseUp = getPointedSun(pointedTargets, inst.params.sunPointerTargetTag.value);
+                        if (sunOnMouseUp && sunOnMouseUp.gameObject.id === pointedSun.gameObject.id) {
+                            console.log('Sun Collected');
+                            return;
+                        }
+                    }
+                }
+            }
+        };
+        return inst;
+    }
+};
+
+module.exports = collectSun;
