3005e3120c7ad1d9b16065a3e95e718d3728aa0f
Vasil <magaranov@abv.bg>
2019-06-27 01:30:33 +0300

    animator migration to program

--- Server/Assets/Scripts/Animation/animator.js
+++ Server/Assets/Scripts/Animation/animator.js
@@ -1,14 +1,11 @@
 var animator = {
+    extendsFrom: 'Assets\\Scripts\\ProgramsAPI\\program.js',
     onLoad: function () {
         game.dev.animation.animator = animator;
     },
     createInstance: function () {
         var inst = {
             name: 'Animator',
-            currentAnimation: undefined,
-            currentAnimationID: undefined,
-            animationProgress: 0,
-            toChange: undefined,
             params: {
                 defaultAnimation: {
                     name: 'Default Animation',
@@ -22,10 +19,14 @@ var animator = {
                 }
             },
             interface: {
+                currentAnimation: undefined,
+                currentAnimationID: undefined,
+                animationProgress: 0,
+                toChange: undefined,
                 playAnimation: function (inst, animation) {
-                    inst.toChange = animation;
+                    inst.interface.toChange = animation;
                 },
-                update: function (inst, dt) {
+                coroutine: function* (inst) {
                     function moveOnInsideComponentProperties(param, propName) {
                         var res = param;
 
@@ -41,72 +42,77 @@ var animator = {
                         return res;
                     }
 
-                    if (inst.toChange) {
-                        inst.currentAnimationID = inst.toChange;
-                        var anim = game.library[inst.currentAnimationID].scriptableObject.component.instance;
-                        inst.currentAnimation = anim;
-                        inst.animationProgress = 0;
-                    }
-
-                    inst.toChange = undefined;
-
-                    if (inst.currentAnimation && inst.currentAnimation.interface.getDuration(inst.currentAnimation) < inst.animationProgress) {
-                        if (inst.currentAnimation.params.looped.value === 0) {
-                            inst.currentAnimation = undefined;
-                            inst.currentAnimationID = undefined;
+                    while (true) {
+                        if (inst.interface.toChange) {
+                            inst.interface.currentAnimationID = inst.interface.toChange;
+                            var anim = game.library[inst.interface.currentAnimationID].scriptableObject.component.instance;
+                            inst.interface.currentAnimation = anim;
+                            inst.interface.animationProgress = 0;
                         }
-                        inst.animationProgress = 0;
-                    }
 
-                    if (!inst.currentAnimation) {
-                        return;
-                    }
+                        inst.interface.toChange = undefined;
 
-                    var animationProperties = inst.currentAnimation.params.properties.value;
-                    for (var i = 0; i < animationProperties.length; ++i) {
-                        var curProp = animationProperties[i];
-                        var go = inst.gameObject;
-                        if (curProp.value.goPath.value !== '') {
-                            var path = curProp.value.goPath.value.split(',');
-                            for (var j = 0; j < path.length; ++j) {
-                                go = go.children[parseInt(path[j])];
+                        if (inst.interface.currentAnimation && inst.interface.currentAnimation.interface.getDuration(inst.interface.currentAnimation) < inst.interface.animationProgress) {
+                            if (inst.interface.currentAnimation.params.looped.value === 0) {
+                                inst.interface.currentAnimation = undefined;
+                                inst.interface.currentAnimationID = undefined;
                             }
+                            inst.interface.animationProgress = 0;
                         }
-                        var component = curProp.value.component;
-                        component = game.scripts[component.value];
 
-                        component = game.api.getComponent(go, component);
-
-                        var paramPath = curProp.value.propertyPath.value;
-                        paramPath = paramPath.split('.');
-                        var param = component.params;
-
-                        for (var j = 0; j < paramPath.length; ++j) {
-                            param = moveOnInsideComponentProperties(param, paramPath[j]);
+                        if (!inst.interface.currentAnimation) {
+                            yield;
+                            continue;
                         }
-                        var anim = curProp.value.propertyAnimation.value;
-                        anim = game.library[anim].scriptableObject.component.instance;
-                        param.value = anim.interface.getValue(anim, inst.animationProgress);
-                    }
 
-                    if (inst.currentAnimation && inst.currentAnimation.params.events.value.script.value) {
-                        var eventReceiverComponent = inst.currentAnimation.params.events.value.script.value;
-                        eventReceiverComponent = game.library[eventReceiverComponent];
-                        eventReceiverComponent = game.api.getComponent(inst.gameObject, eventReceiverComponent);
+                        var animationProperties = inst.interface.currentAnimation.params.properties.value;
+                        for (var i = 0; i < animationProperties.length; ++i) {
+                            var curProp = animationProperties[i];
+                            var go = inst.gameObject;
+                            if (curProp.value.goPath.value !== '') {
+                                var path = curProp.value.goPath.value.split(',');
+                                for (var j = 0; j < path.length; ++j) {
+                                    go = go.children[parseInt(path[j])];
+                                }
+                            }
+                            var component = curProp.value.component;
+                            component = game.scripts[component.value];
 
-                        if (eventReceiverComponent) {
-                            for (var i = 0; i < inst.currentAnimation.params.events.value.eventsToRaise.value.length; ++i) {
-                                var cur = inst.currentAnimation.params.events.value.eventsToRaise.value[i].value;
-                                if (cur.keyNumber.value === inst.animationProgress) {
-                                    eventReceiverComponent.interface.addEvent(eventReceiverComponent, cur.data.value);
+                            component = game.api.getComponent(go, component);
+
+                            var paramPath = curProp.value.propertyPath.value;
+                            paramPath = paramPath.split('.');
+                            var param = component.params;
+
+                            for (var j = 0; j < paramPath.length; ++j) {
+                                param = moveOnInsideComponentProperties(param, paramPath[j]);
+                            }
+                            var anim = curProp.value.propertyAnimation.value;
+                            anim = game.library[anim].scriptableObject.component.instance;
+                            param.value = anim.interface.getValue(anim, inst.interface.animationProgress);
+                        }
+
+                        if (inst.interface.currentAnimation && inst.interface.currentAnimation.params.events.value.script.value) {
+                            var eventReceiverComponent = inst.interface.currentAnimation.params.events.value.script.value;
+                            eventReceiverComponent = game.library[eventReceiverComponent];
+                            eventReceiverComponent = game.api.getComponent(inst.gameObject, eventReceiverComponent);
+
+                            if (eventReceiverComponent) {
+                                for (var i = 0; i < inst.interface.currentAnimation.params.events.value.eventsToRaise.value.length; ++i) {
+                                    var cur = inst.interface.currentAnimation.params.events.value.eventsToRaise.value[i].value;
+                                    if (cur.keyNumber.value === inst.interface.animationProgress) {
+                                        eventReceiverComponent.interface.addEvent(eventReceiverComponent, cur.data.value);
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    ++inst.animationProgress;
+                        ++inst.interface.animationProgress;
+                        yield;
+                    }
                 },
                 start: function(inst) {
+                    game.api.startProgram(inst, {});
                     if (inst.params.playAnimationAtStart.value === 1) {
                         inst.interface.playAnimation(inst, inst.params.defaultAnimation.value);
                     }
